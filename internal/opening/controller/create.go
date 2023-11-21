d01a2d057282d9df08f90577e2a9977c3747b0a5
Jacson Rodrigues <jacson.s.r1@gmail.com>
2023-11-21 06:29:55 +0000

    feat: add swago swagger docs

--- internal/opening/controller/create.go
+++ internal/opening/controller/create.go
@@ -8,6 +8,15 @@ import (
 	"github.com/jacksrm/gopportunitties/internal/opening/dto"
 )
 
+// @BasePath /api/v1/openings
+// @Description Create a new opening
+// @Accept json
+// @Produce json
+// @Param opening body dto.CreateOpening true "Opening object that needs to be added"
+// @Success 201 {object} dto.RequestResponse
+// @Failure 400 {object} dto.RequestResponse
+// @Router /opening [post]
+
 func (c *Controller) Create(context *gin.Context) {
 	const operationName = "CreateOpening"
 	data := dto.CreateOpening{}
--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,18 +1,25 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/jacksrm/gopportunitties/internal/opening"
+
+	"github.com/gin-gonic/gin"
+	docs "github.com/jacksrm/gopportunitties/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
-	v1 := router.Group("/api/v1/")
-
+	const basePath = "/api/v1"
+	v1 := router.Group(basePath)
 	opening := opening.New()
+	docs.SwaggerInfo.BasePath = basePath
 
 	v1.GET("/openings", opening.Controller.GetAll)
 	v1.GET("/opening/:id", opening.Controller.Get)
 	v1.POST("/opening", opening.Controller.Create)
 	v1.PUT("/opening/:id", opening.Controller.Update)
 	v1.DELETE("/opening/:id", opening.Controller.Delete)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
